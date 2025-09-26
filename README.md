7275897c12d87647f0ede572c4647f28402a81fa
Jihyeok Park <jihyeok_park@korea.ac.kr>
2025-09-26 12:10:01 +0900

    Updated

---
+++ README.md
@@ -0,0 +1,8 @@
+# VAE - AE with Variables
+
+You can download this template using [`sbt`](https://www.scala-sbt.org/):
+```bash
+sbt new ku-plrg-classroom/vae.g8
+```
+
+Please see the [documentation](https://github.com/ku-plrg-classroom/docs/tree/main/cose212/vae) for more details.
---
+++ src/main/g8/src/main/scala/kuplrg/Implementation.scala
@@ -0,0 +1,17 @@
+package kuplrg
+
+object Implementation extends Template {
+
+  import Expr.*
+
+  def interp(expr: Expr, env: Env): Value = ???
+
+  def freeIds(expr: Expr): Set[String] = ???
+
+  def bindingIds(expr: Expr): Set[String] = ???
+
+  def boundIds(expr: Expr): Set[String] = ???
+
+  def shadowedIds(expr: Expr): Set[String] = ???
+
+}
